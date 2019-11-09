6be01bbe105fdced092a420dd460f1bf8f3c8d78
PiriyaV <piriya_v@gpo.or.th>
2019-11-09 12:11:29 +0700

    Display title

--- AgentConsoleApp/Program.cs
+++ AgentConsoleApp/Program.cs
@@ -37,6 +37,9 @@ namespace AgentConsoleApp
             int counterLine;
 
             // Display title
+            Console.Title = "TxtToDB 1.01";
+
+            // Display header
             Console.WriteWithGradient(FiggleFonts.Banner.Render("txt to db"), Color.LightGreen, Color.ForestGreen, 16);
             Console.ReplaceAllColorsWithDefaults();
 
