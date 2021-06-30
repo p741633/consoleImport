8ae455964102546fdb1730c9083cc69a9c7d5a7c
PiriyaV <piriya_v@gpo.or.th>
2021-06-30 14:26:59 +0700

    Update version

--- AgentConsoleApp/Program.cs
+++ AgentConsoleApp/Program.cs
@@ -58,7 +58,7 @@ namespace AgentConsoleApp
             if (args.Length == 0)
             {
                 // Display title
-                Console.Title = "TxtToDB 1.0.5";
+                Console.Title = "TxtToDB 1.0.6";
 
                 // Display header
                 Console.WriteWithGradient(FiggleFonts.Banner.Render("txt to db"), Color.LightGreen, Color.ForestGreen, 16);
